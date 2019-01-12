12fd49ee441f86223d7c7c6a75c8c4f5fa7a22e9
Łukasz Danilewicz <l.danilewicz@gmail.com>
2019-01-12 16:59:03 +0100

    Copy test_.py files to newly created folder 'tests'.

---
+++ tests/test_factors.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+#import sys
+#sys.path.append('../tests')
+
+from ..factors import *
+import pytest
+'''
+@pytest.mark.parametrize("name,unit,type,min_val,max_val,alert_min_val,alert_max_val,expected", [
+    ("Temperatura","C","w",-273,100,-10,30)
+])
+'''
+#factors.check_limits() test
+@pytest.mark.parametrize("temperature_limit,expected", [
+    (-300,False),
+    (0,True),
+    (2000,False),
+    (None,False),
+])
+def test_factor_limits(temperature_limit,expected):
+	T = WeatherFactors("Temperatura","C","w",-273.15,1000,-10,30)
+	assert T.check_limits(temperature_limit) == expected
+
+#factors.check_alerts() test
+@pytest.mark.parametrize("temperature_alert,expected", [
+    (-12,True),
+    (-10.1,True),
+    (-10,False),
+    (0,False),
+    (30,False),
+    (30.01,True),
+    (33,True),
+    (None,False),
+])
+def test_check_alerts(temperature_alert,expected):
+	T = WeatherFactors("Temperatura","C","w",-273,100,-10,30)
+	assert T.check_alerts(temperature_alert) == expected
+
+#factors._convert_to_float() test
+@pytest.mark.parametrize("float_value,expected", [
+    (-12,-12),
+    (0,0),
+    ("",None),
+    (None,None),
+    (-30,-30),
+])
+def test__convert_to_float(float_value,expected):
+	T = WeatherFactors("Temperatura","C","w",-273,100,-10,30)
+	assert T._convert_to_float(float_value) == expected
+
---
+++ tests/test_ui_functions.py
@@ -0,0 +1,55 @@
+from ..ui_functions import *
+import pytest
+
+
+@pytest.mark.parametrize("test_data_from_user, test_MIN_expected_value, test_MAX_expected_value, expected_output",
+                         [
+                             ('23.4', 0, 90, (1, 1, 23.4)),  # prawidłowa wartość - OK
+                             ('55,7', 0, 90, (1, 1, 55.7)),  # przecinek zamiast kropki - OK
+                             ('134', 0, 90, (1, 0, None)),  # wartość poza zakresem - BŁĄD
+                             ('-1', 0, 90, (1, 0, None)),  # wartość poza zakresem - BŁĄD
+                             ('deed', 0, 90, (0, 0, None))  # słowo - BŁĄÐ
+                         ]
+                         )
+def test_check_GPS_value_from_user(test_data_from_user, test_MIN_expected_value, test_MAX_expected_value,
+                                   expected_output):
+    result = check_GPS_value_from_user(test_data_from_user, test_MIN_expected_value, test_MAX_expected_value)
+    assert result == expected_output
+
+
+# region test of check_user_choice_is_correct(data_from_user, *args)
+def test_check_user_choice_is_correct_param1():
+    result = check_user_choice_is_correct("4", "1", "2", "3")
+    assert result is None
+
+
+def test_check_user_choice_is_correct_param2():
+    result = check_user_choice_is_correct(3, "1", "2", "3", "4")
+    assert result is None
+
+
+def test_check_user_choice_is_correct_param3():
+    result = check_user_choice_is_correct("3", 1, 2, 3, 4)
+    assert result is None
+
+
+def test_check_user_choice_is_correct_param4():
+    result = check_user_choice_is_correct("3", "1", "2", "3", "4")
+    assert result == "3"
+
+
+def test_check_user_choice_is_correct_param5():
+    result = check_user_choice_is_correct("two", "one", "two", "three", "four")
+    assert result == "two"
+
+
+def test_check_user_choice_is_correct_param6():
+    result = check_user_choice_is_correct("two", "one", "two", "three", "two", "four")
+    assert result == "two"
+
+
+def test_check_user_choice_is_correct_param7():
+    result = check_user_choice_is_correct(3, 1, 2, 3, 4, 5)
+    assert result == 3
+
+# endregion
