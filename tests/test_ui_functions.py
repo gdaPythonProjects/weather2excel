09215a4718f18fdf193b15a7470c17af08c17545
Rafal Glombiowski <rafal.glombiowski@interia.pl>
2019-01-24 08:36:35 +0100

    Change tests for check_GPS_value_form_user to testing negative values

--- tests/test_ui_functions.py
+++ tests/test_ui_functions.py
@@ -4,11 +4,12 @@ import pytest
 
 @pytest.mark.parametrize("test_data_from_user, test_MIN_expected_value, test_MAX_expected_value, expected_output",
                          [
-                             ('23.4', 0, 90, (1, 1, 23.4)),  # prawidłowa wartość - OK
-                             ('55,7', 0, 90, (1, 1, 55.7)),  # przecinek zamiast kropki - OK
-                             ('134', 0, 90, (1, 0, None)),  # wartość poza zakresem - BŁĄD
-                             ('-1', 0, 90, (1, 0, None)),  # wartość poza zakresem - BŁĄD
-                             ('deed', 0, 90, (0, 0, None))  # słowo - BŁĄÐ
+                             ('23.4', -90, 90, (1, 1, 23.4)),  # prawidłowa wartość - OK
+                             ('55,7', -90, 90, (1, 1, 55.7)),  # przecinek zamiast kropki - OK
+                             ('-45', -90, 90, (1, 1, -45)),  # ujemne wartości - OK
+                             ('134', -90, 90, (1, 0, None)),  # wartość poza zakresem - BŁĄD
+                             ('-1', -90, 90, (1, 0, None)),  # wartość poza zakresem - BŁĄD
+                             ('deed', -90, 90, (0, 0, None))  # słowo - BŁĄÐ
                          ]
                          )
 def test_check_GPS_value_from_user(test_data_from_user, test_MIN_expected_value, test_MAX_expected_value,
